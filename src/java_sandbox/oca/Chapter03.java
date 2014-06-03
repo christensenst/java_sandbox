1d22d702e7c7b31605ea1167d057fb23aa1dcbd6
Stephen Christensen <stephen.t.christensen@gmail.com>
2014-06-02 23:17:47 -0500

    Add content to Chapter 3

--- src/java_sandbox/oca/Chapter03.java
+++ src/java_sandbox/oca/Chapter03.java
@@ -34,5 +34,37 @@ public class Chapter03 {
         //Use StringBuilder to represent a mutable character string
         
         //Use StringBuffer to represent a thread-safe mutable character string
+        
+        /* Complete set of valid Java assignment operators:
+         * %=, &=, *=, /=, ^=, |=, +=, <<=, =, -=, >>=, >>>=
+        */
+        int a;
+        a = 10;
+        System.out.println(a += 3); //Eq: a = a + 3; Prints: 13
+        a = 15;
+        System.out.println(a -= 3); //Eq: a = a - 3; Prints: 12
+        a = 20;
+        System.out.println(a *= 3); //Eq: a = a * 3; Prints: 60
+        a = 25;
+        System.out.println(a /= 3); //Eq: a = a / 3; Prints: 8
+        a = 30;
+        System.out.println(a %= 3); //Eq: a = a % 3; Prints: 0
+        a = 35;
+        // The bitwise & operator performs a bitwise AND operation.
+        System.out.println(a &= 3); // Binary: 00100011 & 00000011 = 00101011 Prints: 3
+        a = 40;
+        //The bitwise ^ operator performs a bitwise exclusive OR operation.
+        System.out.println(a ^= 3); // Binary: 00101000 ^ 00000011 = 00101011 Prints: 43
+        a = 45;
+        // The bitwise | operator performs a bitwise inclusive OR operation.
+        System.out.println(a |= 3); // Binary: 00101101 | 00000011 = 00101111 Prints: 47
+        //System.out.println(a = a | 3);
+        a = 50;
+        // The signed left shift operator "<<" shifts a bit pattern to the left
+        System.out.println(a <<= 3); //Prints: -112
+        a = 55;
+        System.out.println(a >>= 3); //Prints: 6
+        a = 60;
+        System.out.println(a >>>= 3); //Prints: 7
     }
 }
