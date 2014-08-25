fbaaf8ac4235f992be768d2b1a5e4406e991d0a4
Stephen Christensen <stephen.t.christensen@gmail.com>
2014-08-24 19:29:22 -0500

    Implement state for the undo button for the ceiling fan

--- src/java_sandbox/design_patterns/command/CeilingFanOnCommand.java
+++ src/java_sandbox/design_patterns/command/CeilingFanOnCommand.java
@@ -8,16 +8,26 @@ import java_sandbox.design_patterns.command.receiver.CeilingFan;
 public class CeilingFanOnCommand implements Command {
     
     CeilingFan ceilingFan;
+    int previousSpeed;
     
     public CeilingFanOnCommand(CeilingFan ceilingFan) {
         this.ceilingFan = ceilingFan;
     }
     
     public void execute() {
+        previousSpeed = ceilingFan.getSpeed();
         ceilingFan.high();
     }
     
     public void undo() {
-        ceilingFan.off();
+        if (previousSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (previousSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (previousSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (previousSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
     }
 }
